50dfeff585d5f3da3096ee00c25e813f317a35e8
李铭昕 <715557344@qq.com>
2019-10-24 15:45:14 +0800

    Added component.

---
+++ hyperf.php
@@ -0,0 +1,28 @@
+<?php
+
+use EasySwoole\EasySwoole\Command\CommandRunner;
+
+defined('IN_PHAR') or define('IN_PHAR', boolval(\Phar::running(false)));
+defined('RUNNING_ROOT') or define('RUNNING_ROOT', realpath(getcwd()));
+defined('EASYSWOOLE_ROOT') or define('EASYSWOOLE_ROOT', IN_PHAR ? \Phar::running() : realpath(getcwd()));
+
+$file = EASYSWOOLE_ROOT.'/vendor/autoload.php';
+if (file_exists($file)) {
+    require $file;
+}else{
+    die("include composer autoload.php fail\n");
+}
+
+if(file_exists(EASYSWOOLE_ROOT.'/bootstrap.php')){
+    require_once EASYSWOOLE_ROOT.'/bootstrap.php';
+}
+
+$args = $argv;
+//trim first command
+array_shift($args);
+$esCommands = CommandRunner::getInstance();
+var_dump($esCommands);
+
+if(!empty($ret)){
+    echo $ret."\n";
+}
